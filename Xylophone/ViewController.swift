6fddfccdcbbba6a82edc92e5b5e3dcca79a82161
Pack <packaq3@gmail.com>
2022-08-18 15:35:56 +0700

    End Ep7

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,26 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+       
+        playSound(sounndName: sender.currentTitle!)
+        
+    }
     
-
+    func playSound(sounndName: String) {
+        let url = Bundle.main.url(forResource: sounndName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
